e870bffb10d5237a0a40df23fe25b45717e1258e
GiroFC <igoretofranco@gmail.com>
2025-08-16 13:34:12 -0300

    INIT - DataBase in SQLite EntityFramework Added New Models

--- CRUD/Person/Program.cs
+++ CRUD/Person/Program.cs
@@ -1,9 +1,13 @@
+using Person.Context;
+using Person.Routes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<PersonContext>();
 
 var app = builder.Build();
 
@@ -14,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.PersonRoutes();
+
 app.UseHttpsRedirection();
-
-
 app.Run();
---
+++ CRUD/Person/Routes/PersonRoute.cs
@@ -0,0 +1,11 @@
+using Person.Models;
+
+namespace Person.Routes;
+
+public static class PersonRoute
+{
+    public static void PersonRoutes(this WebApplication app)
+    {
+        app.MapGet("person", () => new PersonModel("Igor"));
+    }
+}
