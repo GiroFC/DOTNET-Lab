6e95772d9e7d0c1d040aaa05f94d03563a392b01
GiroFC <igoretofranco@gmail.com>
2025-08-30 17:03:44 -0300

    NEW - Models of a Library API

---
+++ LibraryAPI/LibraryAPI/Models/AuthorModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryAPI.Models
+{
+    public class AuthorModel
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<BookModel> Books { get; private set; } = new List<BookModel>();
+
+        public AuthorModel(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+        public void UpdateName(string newName)
+        {
+            if (!string.IsNullOrEmpty(newName))
+                Name = newName;
+        }
+    }
+}
---
+++ LibraryAPI/LibraryAPI/Models/LoanModel.cs
@@ -0,0 +1,31 @@
+﻿namespace LibraryAPI.Models
+{
+    public class LoanModel
+    {
+        public Guid Id { get; private set; }
+
+        public Guid UserId { get; private set; }
+        public UserModel? User { get; private set; }
+
+        public Guid BookId { get; private set; }
+        public BookModel? Book { get; private set; }
+
+        public DateTime LoanAt { get; private set; } = DateTime.Now;
+        public DateTime? ReturnDate { get; private set; }
+
+        public LoanModel(UserModel user, BookModel book)
+        {
+            Id = Guid.NewGuid();
+            User = user;
+            UserId = user.Id;
+            Book = book;
+            BookId = book.Id;
+        }
+
+        public void ReturnBook()
+        {
+            ReturnDate = DateTime.Now;
+        }
+
+    }
+}
