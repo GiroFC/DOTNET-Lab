d3518a4e5e5a8ebccf5957de746409e47a9f221e
GiroFC <igoretofranco@gmail.com>
2025-09-07 01:03:11 -0300

    ADD: DTOs and base controller for books

---
+++ LibraryAPI/LibraryAPI/DTOs/Books/BookCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.DTOs.Books
+{
+    public class BookCreateDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public List<Guid> AuthorIds { get; set; } = new List<Guid>();
+    }
+}
---
+++ LibraryAPI/LibraryAPI/DTOs/Books/BookUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.DTOs.Books
+{
+    public class BookUpdateDto
+    {
+        public string? Title { get; set; }
+        public int? ReleaseYear { get; set; }
+        public string? Genre { get; set; }
+        public List<Guid>? AuthorIds { get; set; }
+    }
+}
